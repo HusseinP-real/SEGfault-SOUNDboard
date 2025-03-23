e63044d0eb042ef8172b2d6f3c79ab00f7e1d239
Haosen Peng <hpen0911@uni.sydney.edu.au>
2025-03-23 10:24:32 +0000

    try again with struct makefile save load functions

--- sound_seg.c
+++ sound_seg.c
@@ -8,24 +8,28 @@
 
 struct sound_seg {
     //TODO
-};
+    uint16_t* samples; // array of samples
+    size_t capacity; // judge if the array is full
+    };
 
 // Load a WAV file into buffer
 void wav_load(const char* filename, int16_t* dest){
     // fopen file, read
     FILE* f = fopen(filename, "rb");
-    if (!f) {
-        return;
-    }
+    if (!f) return;
+
     // get size of file fseek + ftell
     fseek(f, 44, SEEK_SET);
+
     //get left data to dest
     fseek(f, 0, SEEK_END);
     long fileSize = ftell(f); // get the position(pointer)
     long dataSize = fileSize - 44;
     fseek(f, 44, SEEK_SET);
+
     // read dataSize to dest
     fread(dest, 1, dataSize, f);
+
     // close file
     fclose(f);
     
@@ -34,6 +38,53 @@ void wav_load(const char* filename, int16_t* dest){
 
 // Create/write a WAV file from buffer
 void wav_save(const char* fname, int16_t* src, size_t len){
+    //open file
+    FILE *f = fopen(fname, "wb");
+    if (!f) return;
+
+    //calculate datasize
+    uint32_t dataSize = (uint32_t)(len * sizeof(int16_t));
+
+    /*
+    RIFF
+    RIFF + chunkSize + Wave
+    chunkSize = total - 8 = 36 + dataSize
+    */
+    uint32_t chunkSize = 36 + dataSize;
+    fwrite("RIFF", 1, 4, f);
+    fwrite(&chunkSize, sizeof(uint32_t), 1, f);
+    fwrite("WAVE", 1, 4, f);
+
+    //fmt fmt + subchunk1Size + PCM
+    fwrite("fmt", 1, 4, f);
+    uint32_t subchunk1Size = 16; // PCM
+    fwrite(&subchunk1Size, sizeof(uint32_t), 1, f);
+
+    uint16_t audioFormat = 1; // PCM not compressed
+    uint16_t numChannels = 1; // 1 mono, 2 stereo
+    uint16_t blockAlign = numChannels * 2; // each sample 2 bytes
+    uint16_t bitsPerSample = 16; // 16bits
+    uint32_t sampleRate = 8000; //8000hz
+    uint32_t byteRate = sampleRate * numChannels * 2; // num of bytes per second
+    
+
+    fwrite(&audioFormat, sizeof(uint16_t), 1, f);
+    fwrite(&numChannels, sizeof(uint16_t), 1, f);
+    fwrite(&sampleRate, sizeof(uint32_t), 1, f);
+    fwrite(&byteRate, sizeof(uint32_t), 1, f);
+    fwrite(&blockAlign, sizeof(uint16_t), 1, f);
+    fwrite(&bitsPerSample, sizeof(uint16_t), 1, f);
+
+
+    //write sub data
+    fwrite("data", 1, 4, f);
+    fwrite(&dataSize, sizeof(uint32_t), 1, f);
+
+    // write data in wav file
+    fwrite(src, sizeof(int16_t), len, f);
+    //close
+    fclose(f);
+
     return;
 }
 
