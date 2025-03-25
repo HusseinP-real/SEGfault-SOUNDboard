7d7feda8447655f67c87fe6cf0cd3d8a9323d759
Haosen Peng <hpen0911@uni.sydney.edu.au>
2025-03-25 00:47:22 +0000

    try makefile and these funcs

--- sound_seg.c
+++ sound_seg.c
@@ -10,7 +10,8 @@ struct sound_seg {
     // //TODO
     uint16_t* samples; // array of samples
     size_t capacity; // judge if the array is full
-    };
+    size_t length; // length of the array
+};
 
 // Load a WAV file into buffer
 void wav_load(const char* filename, int16_t* dest){
@@ -90,31 +91,89 @@ void wav_save(const char* fname, int16_t* src, size_t len){
 
 // Initialize a new sound_seg object
 struct sound_seg* tr_init() {
-    return NULL;
+    struct sound_seg* track = (struct sound_seg*)malloc(sizeof(struct sound_seg));
+    // allocate memory failed
+    if (!track) return NULL;
+
+    // initialize
+    track->samples = NULL;
+    track->capacity = 0;
+    return track;
 }
 
 // Destroy a sound_seg object and free all allocated memory
 void tr_destroy(struct sound_seg* obj) {
+    // if the pointer is null return
+    if (!obj) return;
+
+    // free the memory if its not null
+    if (obj->samples) {
+        free(obj->samples);
+        obj->samples = NULL;
+    }
+
+    free(obj);
+
     return;
 }
 
 // Return the length of the segment
 size_t tr_length(struct sound_seg* seg) {
-    return (size_t)-1;
+    if (!seg) return 0;
+    return seg->length;
+    //return (size_t)-1;
 }
 
 // Read len elements from position pos into dest
 void tr_read(struct sound_seg* track, int16_t* dest, size_t pos, size_t len) {
+    if (!track || !track->samples || !dest) return;
+    if (pos >= track->length) return;
+    size_t can_read = track->length - pos;
+    if (len > can_read) len = can_read;
+    memcpy(dest, track->samples + pos, len * sizeof(int16_t));
     return;
 }
 
 // Write len elements from src into position pos
 void tr_write(struct sound_seg* track, int16_t* src, size_t pos, size_t len) {
+    if (!track || !src) return;
+
+    // if pose is greater than length, set pos as the end of the track
+    if (pos > track->length) pos = track->length;
+    size_t end_pos = pos + len;
+
+    // if end_pos is greater than capacity, reallocate memory
+    if (end_pos > track-> capacity) {
+        size_t new_capacity = end_pos;
+        int16_t* new_samples = (int16_t*)realloc(track->samples, new_capacity * sizeof(int16_t));
+        if (!new_samples) return;
+
+        // update the pointer and capacity
+        track->samples = new_samples;
+        track->capacity = new_capacity;
+    }
+    memcpy(track->samples + pos, src, len * sizeof(int16_t));
+
+    // update the length
+    if (end_pos > track->length) track->length = end_pos;
+
     return;
 }
 
 // Delete a range of elements from the track
 bool tr_delete_range(struct sound_seg* track, size_t pos, size_t len) {
+    if (!track || !track->samples) return false;
+    if (pos >= track->length) return false;
+    size_t available = track->length - pos;
+    if (len > available) len = available;
+
+    size_t remain = track->length - pos - len;
+    memmove(track->samples + pos, 
+        track->samples + pos + len, 
+        remain * sizeof(int16_t));
+
+    track->length -= len;
+    
     return true;
 }
 
